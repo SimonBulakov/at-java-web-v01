376915ff42609d6064aa174555b91baa4fb3a8c7
SimonBulakov <simonad@mail.ru>
2025-11-30 16:22:01 +0300

    Модуль 1. Урок 1. 2025/11/30. Тест 1.1

--- src/test/java/demo/part01/SimpleWikiTest.java
+++ src/test/java/demo/part01/SimpleWikiTest.java
@@ -2,17 +2,22 @@ package demo.part01;
 
 import org.junit.jupiter.api.Test;
 import static com.codeborne.selenide.Condition.text;
-import static com.codeborne.selenide.Selenide.$;
-import static com.codeborne.selenide.Selenide.open;
+import static com.codeborne.selenide.Selenide.*;
+
 public class SimpleWikiTest {
     @Test
     void test01() {
         open("https://ru.wikipedia.org/wiki/Selenium");
         $("body").shouldHave(text("WebDriver"));
     }
-    @Test
+/*    @Test
     void test02() {
         open("https://ru.wikipedia.org/wiki/Selenium");
         $("body").shouldHave(text("Selenide"));
+    }*/
+    @Test
+    void test03() {
+        open("https://slqa.ru/cases/SimpleForm");
+        $("body").shouldHave(text("Томас Фуллер"));
     }
 }
