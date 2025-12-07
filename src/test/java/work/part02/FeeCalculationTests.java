ab4202d20ad65236340125b7f40c764640b14d12
SimonBulakov <simonad@mail.ru>
2025-12-07 14:04:10 +0300

    Модуль 2. Задание задание провокация (страница 74 презентации).

--- src/test/java/work/part02/FeeCalculationTests.java
+++ src/test/java/work/part02/FeeCalculationTests.java
@@ -1,13 +1,21 @@
 package work.part02;
 
 import org.junit.jupiter.api.Test;
+import org.openqa.selenium.By;
+
 import static com.codeborne.selenide.Selenide.*;
 
 public class FeeCalculationTests {
     @Test
-    public void testElementSearchMethods() {
+    public void test01() {
         open("https://slqa.ru/cases/fc/v01/");
-
-
+        $(By.name("sum")).sendKeys("Просто текст");
+        $(By.name("sum")).sendKeys("Еще текст");
+        $(By.name("sum")).clear();
+        $(By.name("sum")).setValue("100");
+        $(By.name("sum")).setValue("222");
+        $(By.name("sum")).type("50000");
+        $(By.name("sum")).type("62600");
+        $(By.name("submit")).click();
     }
 }
