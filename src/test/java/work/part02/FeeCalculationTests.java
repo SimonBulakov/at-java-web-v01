e1738677d09b225e8ab726b1e94b160ac31081a0
SimonBulakov <simonad@mail.ru>
2025-12-07 13:07:35 +0300

    Модуль 2. Задание: копирование класса - в пакет work.part02 из demo.part02 (страница 68 презентации).

---
+++ src/test/java/work/part02/FeeCalculationTests.java
@@ -0,0 +1,13 @@
+package work.part02;
+
+import org.junit.jupiter.api.Test;
+import static com.codeborne.selenide.Selenide.*;
+
+public class FeeCalculationTests {
+    @Test
+    public void testElementSearchMethods() {
+        open("https://slqa.ru/cases/fc/v01/");
+
+
+    }
+}
