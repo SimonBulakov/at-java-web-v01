58e02fc0c1570188be5662afcb69a6d6a20af279
SimonBulakov <simonad@mail.ru>
2025-12-21 13:45:12 +0300

    модуль 7.задание слайд 16 (регистрация рейсов). 1,2(*) - готово

---
+++ src/test/java/work/part07/POMFlightsTests.java
@@ -0,0 +1,159 @@
+package work.part07;
+
+import com.codeborne.selenide.Configuration;
+import com.codeborne.selenide.logevents.SelenideLogger;
+import work.part07.pages.FlightsListPage;
+import work.part07.pages.LoginPage;
+import work.part07.pages.RegistrationPage;
+import work.part07.pages.SearchPage;
+import io.qameta.allure.selenide.AllureSelenide;
+import org.junit.jupiter.api.*;
+
+import static com.codeborne.selenide.Selenide.open;
+import static com.codeborne.selenide.Selenide.sleep;
+import static com.codeborne.selenide.WebDriverRunner.getWebDriver;
+
+
+@TestMethodOrder(MethodOrderer.DisplayName.class)
+public class POMFlightsTests {
+    @BeforeAll
+    static void beforeAll() {
+        SelenideLogger.addListener("allure", new AllureSelenide());
+    }
+
+    @BeforeEach
+    void setUp() {
+        Configuration.browser = "chrome"; //"firefox "
+        open("https://slqamsk.github.io/cases/slflights/v01/");
+        getWebDriver().manage().window().maximize();
+    }
+    // ... Автотесты
+    // 1. Неуспешный логин
+    @Test
+    void test01WrongPassword() {
+        LoginPage loginPage = new LoginPage();
+        loginPage.login("standard_user", "WrongPassword");
+        loginPage.isLoginUnsuccessful();
+    }
+
+    // 2. Не задана дата
+    @Test
+    void test02NoDate() {
+        LoginPage loginPage = new LoginPage();
+        loginPage.login("standard_user", "stand_pass1");
+        loginPage.isLoginSuccessful("Иванов Иван Иванович");
+
+        SearchPage searchPage = new SearchPage();
+        searchPage.search("");
+        searchPage.isDepartureDateEmpty();
+    }
+    // 3. Не найдены рейсы
+    @Test
+    void test03FlightsNotFound() {
+        LoginPage loginPage = new LoginPage();
+        loginPage.login("standard_user", "stand_pass1");
+        loginPage.isLoginSuccessful("Иванов Иван Иванович");
+
+        SearchPage searchPage = new SearchPage();
+        searchPage.search("16.03.2026", "Казань", "Париж");
+
+        FlightsListPage flightsList = new FlightsListPage();
+        flightsList.isNoFlights();
+    }
+
+    //4. Успешная регистрация с данными по умолчанию
+    @Test
+    void test04SuccessRegistrationDefault() {
+        // Страница логина
+        LoginPage loginPage = new LoginPage();
+        loginPage.login("standard_user", "stand_pass1");
+        loginPage.isLoginSuccessful("Иванов Иван Иванович");
+
+        // Страница поиска рейсов
+        SearchPage searchPage = new SearchPage();
+        searchPage.search("16.03.2026", "Москва", "Нью-Йорк");
+
+        // Страница со списком найденных рейсов
+        FlightsListPage flightsList = new FlightsListPage();
+        flightsList.registerToFirstFlight();
+
+        // Страница регистрации на рейс
+        RegistrationPage registrationPage = new RegistrationPage();
+        registrationPage.isFlightDataCorrect("Москва", "Нью-Йорк");
+        registrationPage.successDefaultRegistration();
+    }
+
+    // 5. Пустые поля
+    @Test
+    void test05EmptyField() {
+        // Страница логина
+        LoginPage loginPage = new LoginPage();
+        loginPage.login("standard_user", "stand_pass1");
+        loginPage.isLoginSuccessful("Иванов Иван Иванович");
+
+        // Страница поиска рейсов
+        SearchPage searchPage = new SearchPage();
+        searchPage.search("16.03.2026", "Москва", "Нью-Йорк");
+
+        // Страница со списком найденных рейсов
+        FlightsListPage flightsList = new FlightsListPage();
+        flightsList.registerToFirstFlight();
+
+        // Страница регистрации на рейс
+        RegistrationPage registrationPage = new RegistrationPage();
+        registrationPage.isFlightDataCorrect("Москва", "Нью-Йорк");
+        registrationPage.registration("", "", "", "");
+        registrationPage.isErrorFillAllFied();
+    }
+
+    // 6. для даты в прошлом отображается ошибка
+    @Test
+    void test06ExpiredDate() {
+        // Страница логина
+        LoginPage loginPage = new LoginPage();
+        loginPage.login("standard_user", "stand_pass1");
+        loginPage.isLoginSuccessful("Иванов Иван Иванович");
+
+        // Страница поиска рейсов
+        SearchPage searchPage = new SearchPage();
+        searchPage.search("20/12/2025", "Казань", "Париж");
+        searchPage.isDepartureDateExpired();
+    }
+
+    // 7. Цепочка тестов
+    @Test
+    void test07ChainTests(){
+        // Страница логина
+        LoginPage loginPage = new LoginPage();
+        loginPage.login("standard_user", "stand_pass1");
+        loginPage.isLoginSuccessful("Иванов Иван Иванович");
+
+        // Страница поиска рейсов
+        SearchPage searchPage = new SearchPage();
+        searchPage.search("16/03/2026", "Казань", "Париж");
+
+        // Страница со списком найденных рейсов (Рейсы не  найдены)
+        FlightsListPage flightsList = new FlightsListPage();
+        flightsList.isNoFlights();
+
+        //Возврат на страницу поиска и новый поиск
+        searchPage.backButton();
+        searchPage.search("16/03/2026", "Москва", "Нью-Йорк");
+
+        // Страница со списком найденных рейсов и регистрация по первмоу найденному
+        flightsList.registerToFirstFlight();
+
+        // Страница регистрации на рейс - проверка корректности рейса
+        RegistrationPage registrationPage = new RegistrationPage();
+        registrationPage.isFlightDataCorrect("Москва", "Нью-Йорк");
+
+        // Страница регистрации на рейс - ввод данных с пустым паспортом (с проверкой на сообщение об ошибке)
+        registrationPage.registration("Иванов Иван Иванович","", "ivanov@example.com", "+7 (123) 456-7890");
+        registrationPage.isErrorFillAllFied();
+        // Страница регистрации на рейс - ввод корректных данных (с последующей пзавершением регистрации)
+        registrationPage.registration("Иванов Иван Иванович","1234 567890", "ivanov@example.com", "+7 (123) 456-7890");
+        registrationPage.successDefaultRegistration();
+
+    }
+
+}
---
+++ src/test/java/work/part07/pages/SearchPage.java
@@ -0,0 +1,47 @@
+package work.part07.pages;
+
+import com.codeborne.selenide.SelenideElement;
+import io.qameta.allure.Step;
+
+import static com.codeborne.selenide.Condition.text;
+import static com.codeborne.selenide.Selenide.$;
+import static com.codeborne.selenide.Selenide.$x;
+
+public class SearchPage {
+    SelenideElement
+            cityFrom = $("#departureCity"),
+            cityTo = $("#arrivalCity"),
+            departureDate = $("#departureDate"),
+            findButton = $x("//button[.='Найти']"),
+            backButton = $x("//button[.='Новый поиск']"),
+            message = $("#searchMessage");
+
+    @Step("Поиск рейсов (задаём только дату)")
+    public void search(String departureDate) {
+        this.departureDate.setValue(departureDate);
+        this.findButton.click();
+    }
+
+    @Step("Поиск рейсов")
+    public void search(String departureDate, String from, String to) {
+        this.departureDate.setValue(departureDate);
+        this.cityFrom.selectOption(from);
+        this.cityTo.selectOption(to);
+        this.findButton.click();
+    }
+
+    @Step("Возврат на новый поиск")
+    public void backButton() {
+        this.backButton.click();
+    }
+
+    @Step("Проверка, что дата не указана")
+    public void isDepartureDateEmpty() {
+        this.message.shouldHave(text("Пожалуйста, укажите дату вылета."));
+    }
+
+    @Step("Ошибка, что дата дата уже прошла")
+    public void isDepartureDateExpired() {
+        this.message.shouldHave(text("Невозможно осуществить поиск: выбранная дата уже прошла."));
+    }
+}
