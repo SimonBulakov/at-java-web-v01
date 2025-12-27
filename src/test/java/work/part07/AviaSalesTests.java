7f25ec336c21dfe52c65c3f94e778cbee4a745d0
SimonBulakov <simonad@mail.ru>
2025-12-27 23:55:17 +0300

    дз Модуль 7.задание слайд 26 (набор автотестов slflights)+ немного aviasales

--- src/test/java/work/part07/AviaSalesTests.java
+++ src/test/java/work/part07/AviaSalesTests.java
@@ -1,18 +1,13 @@
 package work.part07;
 
-import com.codeborne.selenide.Condition;
 import com.codeborne.selenide.Configuration;
-import com.codeborne.selenide.SelenideElement;
 import com.codeborne.selenide.logevents.SelenideLogger;
 import work.part07.pages.AviasailTopElementPage;
 import io.qameta.allure.selenide.AllureSelenide;
 import org.junit.jupiter.api.*;
 
-import java.time.Duration;
-import java.util.Objects;
 
 import static com.codeborne.selenide.Selenide.*;
-import static com.codeborne.selenide.WebDriverRunner.getWebDriver;
 
 @TestMethodOrder(MethodOrderer.DisplayName.class)
 
@@ -24,46 +19,33 @@ public class AviaSalesTests {
 
     @BeforeEach
     void setUp() {
-        //Configuration.pageLoadTimeout = 60_000; // Увеличиваем время для полной загрузки до 1 минут.
+        Configuration.pageLoadTimeout = 180_000; // Увеличиваем время для полной загрузки до 1 минут.
         Configuration.browser = "chrome"; //"firefox "
+        //webdriver().object().manage().window().maximize();
         open("https://www.aviasales.ru/");
-        getWebDriver().manage().window().maximize();
-
     }
 
     @Test
-    void test01AiroportSelection() {
-
-        /*AviasailTopElementPage atePage = new AviasailTopElementPage();
-        atePage.cookiesUseButton();
+    void test01AiroportFromSelection() {
+        Configuration.pageLoadTimeout = 60_000; // Увеличиваем время для полной загрузки до 1 минут.
+        AviasailTopElementPage atePage = new AviasailTopElementPage();
         atePage.selectAirportFrom("Владивосток");
-        sleep(3000);*/
-
-        //Configuration.pageLoadStrategy = "eager"; // Без полной загрузки не работает
-        Configuration.pageLoadTimeout = 180_000; // Увеличиваем время для полной загрузки до 3 минут.
-        // Страница нестабильная: иногда быстро загружается, иногда требуется больше минуты
-        open("https://aviasales.ru/");
-        SelenideElement se = $("#avia_form_origin-input");
-        se.shouldBe(Condition.interactable, Duration.ofSeconds(30)); // Проверяем, что элемент interactable,
-        // т.е. с ним можно взаимодействовать - добавил, т.к. возникали ошибки, что элемент не interactable
-        se.shouldNotBe(Condition.readonly, Duration.ofSeconds(30)); // Проверяем, что элемент доступен для записи,
-        // т.е. с ним можно взаимодействовать - добавил, т.к. возникали ошибки, что элемент readonly
-        System.out.println(se.getValue());
-        String cityTo = "Новосибирск";
-        se.setValue(cityTo); // Иногда срабатывает прямо так
-        System.out.println(se.getValue());
-        // В цикле мы тупо присваиваем значение, пока оно не присвоится.
-        // Конечно, надо добавить ограничение на число попыток, чтобы не было бесконечного цикла
-        // Например, цикл for от 1 до 100, а потом, если значение не присвоено, то выбрасывать ошибку
-        while (!Objects.equals(se.getValue(), cityTo)) {
-            sleep(1_000);
-            se.click(); // Если кликнуть по этому полю, а потом перед тем, как присваивать значение очистить его,
-            // то через какое-то время поле приходит в состояние, когда ему можно присвоить значение, которое требуется
-            se.setValue("").setValue(cityTo);
-            System.out.println(se.getValue());
-        }
+        sleep(3000);
     }
 
+    @Test
+    void test02AiroporToSelection() {
+        AviasailTopElementPage atePage = new AviasailTopElementPage();
+        atePage.selectAirportTo("Сочи");
+        sleep(3000);
+    }
+
+    @Test
+    void test03setDateWhen() {
+        AviasailTopElementPage atePage = new AviasailTopElementPage();
+        atePage.setDepartureDate("1 января, чт");
+        sleep(3000);
+    }
 
 }
 
--- src/test/java/work/part07/pages/AviasailTopElementPage.java
+++ src/test/java/work/part07/pages/AviasailTopElementPage.java
@@ -1,13 +1,15 @@
 package work.part07.pages;
 
+import com.codeborne.selenide.Condition;
 import com.codeborne.selenide.SelenideElement;
 import io.qameta.allure.Step;
 import org.junit.jupiter.api.Assumptions;
 
+import java.time.Duration;
+
 import static com.codeborne.selenide.Condition.text;
 import static com.codeborne.selenide.Condition.visible;
-import static com.codeborne.selenide.Selenide.$;
-import static com.codeborne.selenide.Selenide.$x;
+import static com.codeborne.selenide.Selenide.*;
 
 public class AviasailTopElementPage {
     SelenideElement
@@ -20,15 +22,33 @@ public class AviasailTopElementPage {
 
     @Step("Нажатие на кнопку подтверждения использовани я куки")
     public void cookiesUseButton() {
-       if (this.cookiesUseButton.isDisplayed()) {
+       this.cookiesUseButton.shouldBe(Condition.interactable, Duration.ofSeconds(30));
+        if (this.cookiesUseButton.isDisplayed()) {
            this.cookiesUseButton.click();
        }
 
     }
 
     @Step("Ввод аэропорта отправления")
-    public void selectAirportFrom(String airportTo) {
-        this.airportFrom.setValue(airportTo);
+    public void selectAirportFrom(String airport) {
+        this.airportFrom.shouldBe(Condition.interactable, Duration.ofSeconds(30));
+        this.airportFrom.click();
+        sleep(3_000);
+        this.airportFrom.setValue("");
+        sleep(3_000);
+        this.airportFrom.type(airport);
+    }
+
+    @Step("Ввод аэропорта прибытия")
+    public void selectAirportTo(String airport) {
+        this.airportTo.shouldBe(Condition.interactable, Duration.ofSeconds(30));
+        this.airportTo.setValue(airport);
+    }
+
+    @Step("Ввод в поле Когда")
+    public void setDepartureDate(String date) {
+        this.departureDate.shouldBe(Condition.interactable, Duration.ofSeconds(30));
+        this.departureDate.sendKeys(date);
     }
 
 
